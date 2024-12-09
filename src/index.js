12bdad59215a016a4f3790e5b9f9cfff0d8fe836
DaniellaOhenewaaAsumadu <you@example.com>
2024-12-09 12:48:54 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let Button=document.querySelector("button");
+function changebutton(){
+Button.style.backgroundColor="blue";
+Button.style.color="white";
+
+}
+Button.addEventListener("click",changebutton);
